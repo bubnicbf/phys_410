66f21030166b27497420c16bc2b05e770e0e96c6
Ben Bubnick <bfbubnic@us.ibm.com>
2019-07-25 17:23:14 -0400

    complex examples

---
+++ complex_class/complex.cc
@@ -0,0 +1,12 @@
+//Program to calculate addition and multiplication of two complex numbers
+using namespacestd;
+#include<iostream>
+#include<cmath>
+#include<complex>
+intmain()
+{
+    complex<double>x(6.1,8.2), y(0.5,1.3);
+    //writeoutx+y
+    cout << x+y << x*y << endl;
+    return0;
+}
---
+++ complex_class/complex.h
@@ -0,0 +1,14 @@
+#ifndef Complex_H
+#define Complex_H
+//various include statements and definitions
+#include <iostream>//Standard ANSI−C++ includefiles
+#include <new>
+#include ....
+
+classComplex
+{...
+	definitionofvariablesandtheircharacter
+};
+//declarationsofvariousfunctionsusedbytheclass
+...
+#endif
