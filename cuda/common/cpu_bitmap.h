c870107c13000260467121e05d7588d3fb9228df
Ben Bubnick <bfbubnic@us.ibm.com>
2019-09-03 10:49:25 -0400

    fixed inside code of display_and_exit the char* pointer, should be a const

--- cuda/common/cpu_bitmap.h
+++ cuda/common/cpu_bitmap.h
@@ -1,3 +1,5 @@
+/* cf. Jason Sanders, Edward Kandrot. CUDA by Example: An Introduction to General-Purpose GPU Programming */
+// I found this copyright notice off of jiekebo's repo:
 /*
  * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
  *
@@ -12,75 +14,76 @@
  * your use of this NVIDIA software.
  *
  */
-
-
+ 
 #ifndef __CPU_BITMAP_H__
 #define __CPU_BITMAP_H__
 
-#include "gl_helper.h"
+#include <GL/glut.h>
+#include <GL/glext.h>
+#include <GL/glx.h>
 
 struct CPUBitmap {
-    unsigned char    *pixels;
-    int     x, y;
-    void    *dataBlock;
-    void (*bitmapExit)(void*);
-
+    unsigned char *pixels;
+    int           x,y;
+    void          *dataBlock;
+    void (*bitmapExit)(void *);
+    
     CPUBitmap( int width, int height, void *d = NULL ) {
-        pixels = new unsigned char[width * height * 4];
+        pixels = new unsigned char[width*height *4];
         x = width;
         y = height;
         dataBlock = d;
     }
-
+    
     ~CPUBitmap() {
         delete [] pixels;
     }
-
-    unsigned char* get_ptr( void ) const   { return pixels; }
+    
+    unsigned char* get_ptr( void ) const { return pixels; }
     long image_size( void ) const { return x * y * 4; }
-
+    
     void display_and_exit( void(*e)(void*) = NULL ) {
-        CPUBitmap**   bitmap = get_bitmap_ptr();
+        CPUBitmap** bitmap = get_bitmap_ptr();
         *bitmap = this;
         bitmapExit = e;
-        // a bug in the Windows GLUT implementation prevents us from
-        // passing zero arguments to glutInit()
-        int c=1;
-        char* dummy = "";
-        glutInit( &c, &dummy );
+        
+        int c = 1;
+        const char* dummy = "";
+        glutInit( &c, &dummy);
         glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA );
-        glutInitWindowSize( x, y );
+        glutInitWindowSize( x, y);
         glutCreateWindow( "bitmap" );
-        glutKeyboardFunc(Key);
-        glutDisplayFunc(Draw);
+        glutKeyboardFunc( Key );
+        glutDisplayFunc( Draw );
         glutMainLoop();
     }
-
-     // static method used for glut callbacks
+    
+    // static method used for glut callbacks
     static CPUBitmap** get_bitmap_ptr( void ) {
-        static CPUBitmap   *gBitmap;
+        static CPUBitmap    *gBitmap;
         return &gBitmap;
     }
-
-   // static method used for glut callbacks
+    
+    // static method used for glut callbacks
     static void Key(unsigned char key, int x, int y) {
         switch (key) {
             case 27:
-                CPUBitmap*   bitmap = *(get_bitmap_ptr());
-                if (bitmap->dataBlock != NULL && bitmap->bitmapExit != NULL)
-                    bitmap->bitmapExit( bitmap->dataBlock );
-                exit(0);
+            CPUBitmap* bitmap = *(get_bitmap_ptr());
+            if (bitmap->dataBlock != NULL && bitmap->bitmapExit != NULL)
+                bitmap->bitmapExit( bitmap->dataBlock );
+            exit(0);
         }
     }
-
+    
     // static method used for glut callbacks
     static void Draw( void ) {
-        CPUBitmap*   bitmap = *(get_bitmap_ptr());
+        CPUBitmap* bitmap = *(get_bitmap_ptr());
         glClearColor( 0.0, 0.0, 0.0, 1.0 );
         glClear( GL_COLOR_BUFFER_BIT );
-        glDrawPixels( bitmap->x, bitmap->y, GL_RGBA, GL_UNSIGNED_BYTE, bitmap->pixels );
+        glDrawPixels( bitmap->x, bitmap->y, GL_RGBA, GL_UNSIGNED_BYTE, bitmap->pixels);
         glFlush();
     }
 };
 
-#endif  // __CPU_BITMAP_H__
+#endif // __CPU_BITMAP_H__
+        
