49a3e02a2f4bd4d6379f6be2e6d45fd66d009389
Ben Bubnick <bfbubnic@us.ibm.com>
2019-12-16 10:59:58 -0500

    Added examples

--- moreCUDA/cudaMallocHost_eg.cu
+++ moreCUDA/cudaMallocHost_eg.cu
@@ -6,7 +6,7 @@
  * @ref    : https://github.com/monotone-RK/cuda-samples/blob/cc5f30f9cf4ad6c2766bfa05536cf7520ff57193/vecadd_simple/main.cu
  * @note   : On Fedora 23 Workstation, I sometimes get Segmentation Fault when N_0 below is
  * // 2048 (for 1<<12 or greater, I obtain a Segmentation Fault)
-    // for a single float array, for size 1<<12 or greater, I obtain a Segmentation Fault
+	// for a single float array, for size 1<<12 or greater, I obtain a Segmentation Fault
  * after exiting Sleep mode.  When I restart my computer, it doesn't have this problem anymore.  
  * 
  * If you find this code useful, feel free to donate directly and easily at this direct PayPal link: 
@@ -20,7 +20,7 @@
  * (or math, sciences, etc.), so I am committed to keeping all my material 
  * open-source and free, whether or not 
  * sufficiently crowdfunded, under the open-source MIT license: 
- *  feel free to copy, edit, paste, make your own versions, share, use as you wish.  
+ * 	feel free to copy, edit, paste, make your own versions, share, use as you wish.  
  *  Just don't be an asshole and not give credit where credit is due.  
  * Peace out, never give up! -EY
  * 
@@ -34,42 +34,42 @@
 #include <iostream>
 
 __global__ void vecadd(float *a, float *b, float *c) {
-    int tx = threadIdx.x + blockDim.x * blockIdx.x ;
-    c[tx] = a[tx] + b[tx];
+	int tx = threadIdx.x + blockDim.x * blockIdx.x ;
+	c[tx] = a[tx] + b[tx];
 }
 
 int main(int argc, char*argv[]) {
-    const int N_0 = (1<<12) ; 
-    std::cout << " N_0 : " << N_0 << std::endl; 
-    
-    float *a; 
-    float *b; 
-    float *c; 
-    
-    
-    cudaMallocHost(&a, N_0*sizeof(float));
-    cudaMallocHost(&b, N_0*sizeof(float));
-    cudaMallocHost(&c, N_0*sizeof(float));
+	const int N_0 = (1<<12) ; 
+	std::cout << " N_0 : " << N_0 << std::endl; 
+	
+	float *a; 
+	float *b; 
+	float *c; 
+	
+	
+	cudaMallocHost(&a, N_0*sizeof(float));
+	cudaMallocHost(&b, N_0*sizeof(float));
+	cudaMallocHost(&c, N_0*sizeof(float));
 
-    
-    for (int idx=0; idx<N_0; idx++) {
-        a[idx] = 1.0f; 
-        b[idx]  = 2.0f;
-        c[idx] = 0.0f;
-    }
-    
-    int M_x = (1<<5); 
-    int N_x = (N_0+M_x-1)/M_x;
-    vecadd<<<N_x,M_x>>>(a,b,c);
-    
-    for (int idx=0; idx<N_0; idx++) {
-        std::cout << " idx : " << idx << ", c[idx] : " << c[idx] << std::endl; 
-    }
-    
-    
-    cudaFreeHost(a);
-    cudaFreeHost(b);
-    cudaFreeHost(c);
-    
-    return 0;
+	
+	for (int idx=0; idx<N_0; idx++) {
+		a[idx] = 1.0f; 
+		b[idx]  = 2.0f;
+		c[idx] = 0.0f;
+	}
+	
+	int M_x = (1<<5); 
+	int N_x = (N_0+M_x-1)/M_x;
+	vecadd<<<N_x,M_x>>>(a,b,c);
+	
+	for (int idx=0; idx<N_0; idx++) {
+		std::cout << " idx : " << idx << ", c[idx] : " << c[idx] << std::endl; 
+	}
+	
+	
+	cudaFreeHost(a);
+	cudaFreeHost(b);
+	cudaFreeHost(c);
+	
+	return 0;
 }
